9655c872c53c1869250dda9c1759de2e330261b9
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-30 11:53:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+const button = document.getElementById("button");
+
+button.addEventListener('click', (event)=>{
+	event.preventDefault();
+	var name =   document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+	// Construct the query string based on the input values
+      var queryString = "";
+
+      if (name && year) {
+        queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+      } else if (name) {
+        queryString = "?name=" + encodeURIComponent(name);
+      } else if (year) {
+        queryString = "?year=" + encodeURIComponent(year);
+      }
+
+	
+	var h3 = document.getElementById("url");
+	h3.textContent = "https://localhost:8080/" + queryString;
+});
